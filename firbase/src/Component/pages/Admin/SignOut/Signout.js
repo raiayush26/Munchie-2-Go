3bc4cb5077edddba3a8c365e62c60bae1bc50ce2
ayush2020 <ayush2020@vitbhopal.ac.in>
2024-05-11 20:23:49 +0530

    Some-change

--- firbase/src/Component/pages/Admin/SignOut/Signout.js
+++ firbase/src/Component/pages/Admin/SignOut/Signout.js
@@ -1,12 +1,12 @@
 import {signOut} from 'firebase/auth'
 import { auth } from '../../../firebase';
-import { redirect } from 'react-router-dom';
+// import { redirect } from 'react-router-dom';
 const logout = async()=>{
           try {
           await signOut(auth);
-          alert('Succesful sign out');
+          alert('Successful sign out');
           } catch (error) {
-         
+        
           }
 }
 export default logout
